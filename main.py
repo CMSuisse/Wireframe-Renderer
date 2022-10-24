2da178a4b82cffdd5cfe06b8ae4aa2ce6c6098cb
CMSuisse <cmarti@gmx.net>
2022-10-24 19:12:58 +0200

    Added generic user input

--- main.py
+++ main.py
@@ -25,9 +25,6 @@ rotation_matricies = (  rotation_matrix_x,
                         rotation_matrix_y,
                         rotation_matrix_z)
 
-rotation_axis_select = int(input("Around which axis should the wireframe rotate (0: x, 1: y, 2: z)? "))
-rotation_axis = rotation_matricies[rotation_axis_select]
-
 pygame.init()
 screen_size = (640, 480)
 screen_center = (screen_size[0]//2, screen_size[1]//2)
@@ -37,9 +34,9 @@ clock = pygame.time.Clock()
 
 focal_length = 5
 
-def rotate_vertex_positions(vertex_positions: List) -> List:
+def rotate_vertex_positions(vertex_positions: List, rotation_axis_index: int) -> List:
     #Around what axis the cube should rotate
-    vertex_positions = np.dot(vertex_positions, rotation_axis)
+    vertex_positions = np.dot(vertex_positions, rotation_matricies[rotation_axis_index])
     return vertex_positions
 
 def project_vertex_position(vertex: List) -> List:
@@ -52,9 +49,14 @@ def convert_projected_position_to_screen_coordinates(projected_vertex: List) -> 
     screen_y_coordinates = projected_vertex[1]*50 + screen_center[1]
     return [screen_x_coordinates, screen_y_coordinates]
 
-def loop() -> None:
+def loop(rotation_axis_index: int) -> None:
     global vertex_positions
-    vertex_positions = rotate_vertex_positions(vertex_positions)
+    if auto_rotate:
+        #Rotate around the y-Axis by default
+        vertex_positions = rotate_vertex_positions(vertex_positions, 1)
+    if not auto_rotate and rotation_axis_index != None:
+        vertex_positions = rotate_vertex_positions(vertex_positions, rotation_axis_index)
+
     vertex_screen_positions = []
     screen.fill((255, 255, 255))
 
@@ -65,14 +67,34 @@ def loop() -> None:
     for edge in edge_table:
         pygame.draw.line(screen, (0, 0, 0), vertex_screen_positions[edge[0]], vertex_screen_positions[edge[1]], 2)
 
+def poll_input_keys() -> int:
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_s]:
+        return 0
+    if keys[pygame.K_d]:
+        return 1
+    if keys[pygame.K_e]:
+        return 2
+    else:
+        return None
+
+
 
 active = True
+auto_rotate = True
 while active:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             active = False
-    
-    loop()    
+        if event.type == pygame.KEYDOWN:
+            if event.key == 32 and auto_rotate:
+                auto_rotate = False
+            elif event.key == 32 and not auto_rotate:
+                auto_rotate = True
+
+    rotation_axis_index = poll_input_keys()
+    #print(rotation_axis_index)
+    loop(rotation_axis_index)    
     pygame.display.flip()
     clock.tick(60)
 
